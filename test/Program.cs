311fff9fdafac07a332a0a9727951aebed597749
kirill <nicknamekirill@gmail.com>
2023-04-11 20:51:09 +0300

    finish

--- test/Program.cs
+++ test/Program.cs
@@ -4,3 +4,12 @@ string text = Console.ReadLine();
 M = text.Split(" ");
 var result = new string[M.Length];
 var realSize = 0;
+foreach (var value in M)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(" " , result));
