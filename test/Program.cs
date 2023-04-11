7271f645dba37d6ea8e8c8aaa904a1b800aa983f
kirill <nicknamekirill@gmail.com>
2023-04-11 20:50:16 +0300

    write string array

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string text = Console.ReadLine();
+M = text.Split(" ");
+var result = new string[M.Length];
+var realSize = 0;
